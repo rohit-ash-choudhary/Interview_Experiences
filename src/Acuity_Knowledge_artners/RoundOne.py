8ef566bfd1d5222d9a54213d3231f852b6dc69da
rohit-ash-choudhary <ashhh.choudhary@gmail.com>
2024-01-06 15:58:53 +0530

    Acuity Knowledge Partners

---
+++ src/Acuity_Knowledge_artners/RoundOne.py
@@ -0,0 +1,19 @@
+/*
+Teams Round :
+Python  :
+1. Basic Questions Opps
+2. List vs Tuple Vs Dic
+3. How to change data in tuple
+4. How you decided fill value with in Null value :  fill with median,  mean, avg
+    5.
+
+SQL :
+1. Order of sql query exceution
+2. Normilization
+3. Inner join : all join basic
+4.
+
+
+
+
+*/
---
+++ src/Acuity_Knowledge_artners/RoundTwo.py
@@ -0,0 +1,21 @@
+/* Round : 2 -Telephonic  Round:  Mangerial
+1. Introduction
+2. What funtionality use in day-day to resposibility(functionality)
+3. avg salary of employee by category : IN SQL
+4. delete vs turncate : IF no time constrain of time not also not single to what main difference
+5. Index in sql : Disadvantages
+6.  avg salary of employee by category : how in Pandas
+7.  how to add two data frame in python using the key
+8.  play around database
+9. schema design in datanase
+
+
+
+
+
+
+
+
+
+
+
