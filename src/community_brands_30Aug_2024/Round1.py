03b389a46451c777ca0390a11cbf2d7fac65da4e
rohit-ash-choudhary <YOUR_GITHUB_EMAIL_ADDRESS>
2024-09-02 15:16:37 +0530

    Community Brands

---
+++ src/community_brands_30Aug_2024/Round1.py
@@ -0,0 +1,53 @@
+$ JD :
+We’d love to chat if you…
+• Possess an ingrained commitment to providing excellent client service
+• Love to solve analytical problems
+• Are attentive to detail
+• Enjoy the pace and variety of managing multiple priorities
+• Are skilled written and oral communicators, particularly of technical ideas to nontechnical
+audiences
+• Like to understand how things work and aren't shy about asking questions
+• Are interested in data and
+• Prefer a fun, collaborative work environment.
+And you have...
+• Knowledge of SQL and relational databases. Strong in writing complex and optimized
+queries
+• Good Analytical skill
+• Good Communication skill
+• Good to have a basic knowledge of Python
+• Good to have a basic knowledge of MongoDB
+• Good to have a basic familiarity with the Linux command line
+• Good to have experience using code editors and source control (Git preferred) to build
+• Bachelor’s or Master’s degree in computer science or relevant Engineering degree.
+Good People, Doing Good Things:
+Employees at Community Brands are techies and volunteers who strive to make the Company a
+great place to work. We dream big and are motivated to help our customers use the technology
+we create to improve the world around us. And we look forward to you being part of our story!
+• Planned Paid Time Off
+• Purpose Driven Culture
+• Work-life balance
+• Passionate about Community Involvement
+• Company Paid Parental Leave
+• Remote Flexibility
+"""
+
+Level : too easy
+
+
+Its about mostly SQL and datbase concept :
+1. Type of keys
+2. Union and Union All
+3. Procedure and Function
+4. SQL Query : give sql to write : level : medium
+5. Python basic questions
+6. Some DBMS Concept
+7. Types of joins
+8. left out join
+9. Python is case sentive or not
+
+
+
+
+
+
+"""
