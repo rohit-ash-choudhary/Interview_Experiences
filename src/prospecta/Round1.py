1af140fe2e543816dfb174f9bcd955ee5a697d17
Rohitash . <choudhar@amdocs.com>
2025-04-29 17:02:33 +0530

    PROSPECTA

---
+++ src/prospecta/Round1.py
@@ -0,0 +1,11 @@
+1. Backround working of index how its work m difference between clustered & non-clustred index
+   with backgroud working
+2. does index effect the data base storage
+3. how to create index
+4. Query for duplicate in a table
+5. Group by employee from department
+6. Ashyroinoys connectioin in spring
+7. Types of thread in java
+8. what is difference between HIBERNATE & JDBC TEMPLATE
+9. why we use the hibernate over jdbc template
+10.
