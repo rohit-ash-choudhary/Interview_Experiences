02e79d595466f674cf1e3ec0fbe6410dd253730a
Rohitash . <choudhar@amdocs.com>
2025-05-05 16:12:56 +0530

    Appean Round 1

---
+++ src/Appen_5May_2025/Round1.py
@@ -0,0 +1,43 @@
+1. WHAT hascode method do, what if return same value , so happened with that data structure
+
+2. @conditionalonBean
+
+3. Circular dependency of bean how you resolve this.
+
+4. Memory leak how you identify the memory leak code this is memory leak code
+
+5. JIT Compiler
+
+6. how to optimize sol query this is good query
+
+8. All conditional statement in Spring
+
+7. Invert the binary tree.
+
+
+/*
+
+Given the root of a binary tree, invert the tree, and return its root. Inverting a binary tree involves swapping each node's left and right subtrees recursively.
+
+4
+
+/ \
+ \
+2   7
+
+/ \ / \
+ \
+    1  3 6  9
+
+4
+
+/ \
+ \
+7   2
+
+/ \ / \
+ \
+    9  6 3 1
+
+*/
+
