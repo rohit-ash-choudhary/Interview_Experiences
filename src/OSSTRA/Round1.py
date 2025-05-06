953b2dcc4df11595eafd3b4ee59c637291fb4315
Rohitash . <choudhar@amdocs.com>
2025-05-06 16:38:01 +0530

    OSSTRA 2 out 5

---
+++ src/OSSTRA/Round1.py
@@ -0,0 +1,25 @@
+1. take linked list have duplicate value linked list is sorted you need to remove duplicate from that
+2. Write Controller code for using parameter method
+3. if two service are same you can use auto wired them how you want first one like @Quilifier annotation
+4. if you a method in controller class you want to search only 5 name through that rest what to block then what you will use
+like in database many user you want to allow only 5 name in that to access or call that method in so what you will use
+5. Employee class
+    public class EmployeerContor {
+
+
+Employee emp1=new Employee("Rohit",26);
+
+
+Employee emp12=new Employee("Rohit",26);
+
+
+
+
+
+}
+Now you want to compare these two employee method are equals if you want to proof that so what you will do because its value is different from ascode according to equals method.
+
+how you proof that 2 method are equals
+
+
+6. Exception in method , what responce you sent in responce method git
