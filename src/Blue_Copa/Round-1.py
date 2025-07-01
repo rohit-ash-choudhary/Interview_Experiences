fda50a4d299517e5f48442ec3de566acab71e6f2
Rohitash . <choudhar@amdocs.com>
2025-07-01 14:20:48 +0530

    The Blue-Copa & Newgen

---
+++ src/Blue_Copa/Round-1.py
@@ -0,0 +1,62 @@
+Array={3,4,5,5,5}
+
+num_present_more_nby2times=1;
+for:i to arr.legtnh=1;
+arr[i]^num_present_more_nby2times=
+
+
+
+def find_number_morethen_nthbytwo(array)
+    number=None
+    count=0
+
+    for i in array:
+        if count=0;
+        number=i;
+        count=1;
+    elif i==number:
+    count+=1;
+else:
+count=-1;
+
+
+
+
+s ="abcdabcbb" ;  //longest sub string length
+0,1,2,3,4,5,6,7
+
+
+def longest_substring(s):
+    char_index={a:4,b:8,c:5,d:3} //3
+    left=0; //0  , right =0
+    max_length=0;
+    for right in range(len(s)):
+        s[right] in char_index and char_index[s[right]]>=left:
+        left=char_index[s[right]]+1
+    char_index[s[right]]=right
+    max_length=max(max_length,right-left+1)
+return max_length;
+
+
+
+Employee :
+select max(salary) from employee where salary < (select max(salary) from employee where salary {})
+
+select salary from employee order by salary desc limit offset 4;
+
+
+Order :
+
+delete from order where order_date < SYSDATE-365*10;  //DATE.FORMAT("MM-DD-YYYY");
+
+update order set is_delete="True" where  order_date < SYSDATE-365*10;  //DATE.FORMAT("MM-DD-YYYY");
+
+
+ REST API AND THEIR METHOD
+REST VS SOAP
+
+ANNOTATION IN SPRING
+
+JWT AUTHENTICATION
+
+OOPS CONCPET
---
+++ src/Newgen/Round1.py
@@ -0,0 +1,104 @@
+Input: {10, 3, 5, 6, 2}
+Output: {180, 600, 360, 300, 900}
+
+Public Void ProductArray(int[] input)
+{
+
+    Int[] output={};
+Int Product=1;
+for(int i=0;i<input.length;i++)/ /10, 3
+{
+
+for(int j=0;j<input.length;j++)  //10,3,5,6,2
+{
+
+if(arr[i]==arr[j])
+{
+continue;
+
+}
+Else{
+
+product*=arr[j];
+}
+output.append(product);
+}
+}
+
+}
+
+
+i/p - “this     is a    string”
+o/p - “siht    sa a   gnirts”
+
+
+
+Public String reverse(String ip)
+{
+
+
+
+    String[] words=ip.split(“ “);
+
+StringBuilder rever_string=new StringBuilder ();
+for(int i=0;i<words.length;i++)
+{
+    String reverse_words=””;
+for(int j=words[i].length-1;j>0;j–)
+{
+
+    reverse_words+=words[j];
+}
+reverse_words+=” ‘;
+rever_string+=reverse_words;
+
+
+}
+Return rever_string;
+
+}
+
+
+
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Hello World" + check());
+}
+
+public static int check() {
+try {
+return 1;
+} finally {
+return 0;
+}
+}
+}
+
+
+class A {}
+class B {}
+class C {}
+class Main {
+static void display(A a) {
+System.out.println("In A");
+}
+static void display(B b) {
+System.out.println("In B");
+}
+static void display(C c) {
+System.out.println("In C");
+}
+public static void main(String[] args) {
+display(null);
+}
+}
+
+
+uSE  of Execsutive service ;cyclie linkedin , black queue, and runtime advantage of lamda and why we use default and static in interface why we needed method definition in interface,
+
+
+String a=”abc” _+ LinkededHashMap Manage order :
+
+
+why enum of static final keytword
+why is advantes of enum over static final variables
